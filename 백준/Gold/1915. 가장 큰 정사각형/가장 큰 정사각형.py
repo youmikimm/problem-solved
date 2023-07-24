eca2dc82918abf55717277671efdecbd12692d46
Youmi Kim <99166914+imyoumikim@users.noreply.github.com>
2023-07-25 04:10:46 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 1200 ms, Memory: 56572 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+map = [[0 for _ in range(M+1)] for _ in range(N+1)]
+length = 0
+
+for i in range(1, N+1):
+    tmp = [0] + list(input().strip())
+    for j in range(1, M+1):
+        map[i][j] = int(tmp[j])
+        if map[i][j] == 1:
+            map[i][j] = min(map[i-1][j-1], map[i][j-1], map[i-1][j]) + 1
+        length = max(length, map[i][j])
+
+print(length * length)
