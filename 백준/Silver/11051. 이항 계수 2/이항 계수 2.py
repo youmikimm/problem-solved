453e3d3f6b7a9484ba9dc46f0643fa24d63e439d
Youmi Kim <99166914+imyoumikim@users.noreply.github.com>
2023-07-31 15:00:47 +0900

    [Silver II] Title: 이항 계수 2, Time: 304 ms, Memory: 54508 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+MOD = 10007
+dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(n+1):
+    dp[i][0] = 1
+    dp[i][1] = i % MOD
+    dp[i][i] = 1
+    
+for i in range(2, n+1):
+    for j in range(2, i):
+        dp[i][j] = ((dp[i-1][j] % MOD) + (dp[i-1][j-1] % MOD)) % MOD
+        
+print(dp[n][k])
