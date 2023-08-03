7199bc72a804a9f87228f12fb226f83f277683d7
Youmi Kim <99166914+imyoumikim@users.noreply.github.com>
2023-08-04 01:49:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,6 +1,2 @@
 def solution(s):
-    if len(s) % 2 == 0:
-        mid = len(s) // 2
-        return s[mid-1:mid+1]
-    else:
-        return s[len(s)//2]
+    return s[(len(s)-1)//2 : len(s)//2+1]
