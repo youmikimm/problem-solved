b33174ed68f708b1bddc9b7ba6bc21156bb89fcf
Youmi Kim <99166914+imyoumikim@users.noreply.github.com>
2023-09-02 23:29:55 +0900

    [level 2] Title: 짝지어 제거하기, Time: 193.92 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = [s[0]]
+    
+    for i in range(1, len(s)):
+        if len(stack) > 0 and s[i] == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+        
+    if len(stack) == 0:
+        return 1
+    return 0
