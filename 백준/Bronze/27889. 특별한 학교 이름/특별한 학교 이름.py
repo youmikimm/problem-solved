e67b3efd5cd7791ae3f312b93238199c5a0aa25a
Youmi Kim <99166914+youmikimm@users.noreply.github.com>
2023-10-17 22:54:11 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,4 @@
+names = {'NLCS': 'North London Collegiate School', 'BHA': 'Branksome Hall Asia', 
+        'KIS': 'Korea International School', 'SJA': 'St. Johnsbury Academy'}
+
+print(names[input().strip()])
