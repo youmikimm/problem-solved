30dd4944e7cce6817b8adaa52732f1ae84eb2893
Youmi Kim <99166914+youmikimm@users.noreply.github.com>
2023-11-19 09:40:41 +0900

    [D3] Title: 팔씨름, Time: 148 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D3/13547. 팔씨름/README.md
@@ -0,0 +1,15 @@
+# [D3] 팔씨름 - 13547 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AX6PP9G6p1sDFAS9) 
+
+### 성능 요약
+
+메모리: 58,260 KB, 시간: 148 ms, 코드길이: 916 Bytes
+
+### 제출 일자
+
+2023-11-19 09:40
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13547. 팔씨름/팔씨름.py
@@ -0,0 +1,31 @@
+
+
+
+
+'''
+      아래의 구문은 input.txt 를 read only 형식으로 연 후,
+      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+
+      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+
+      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+'''
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+def fight_result(string):
+    lst = list(string)
+    cnt = 0
+    for x in lst:
+        if x == 'x':
+            cnt += 1
+        if cnt > 7:
+            return "NO"
+    return "YES"
+
+T = int(input())
+for tc in range(1, T + 1):
+    print(f'#{tc} {fight_result(input())}')
