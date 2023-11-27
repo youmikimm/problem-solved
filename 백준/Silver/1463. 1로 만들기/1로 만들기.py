50992b37cca06204c516d8836f4b8f111ed8c46d
Youmi Kim <99166914+youmikimm@users.noreply.github.com>
2023-11-27 22:45:26 +0900

    [Silver III] Title: 1로 만들기, Time: 548 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -3,13 +3,12 @@ input = sys.stdin.readline
 
 N = int(input())
 dp = [0] * (N+1)
-dp[1] = 0
 
 for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1    # -1 연산 1회 카운트
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)    # /2 연산 1회 카운트
+    dp[i] = dp[i-1] + 1
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)    # /3 연산 1회 카운트
-        
+        dp[i] = min(dp[i//3] + 1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2] + 1, dp[i])
+    
 print(dp[N])
