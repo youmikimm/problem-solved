5aa63ab65b15de0edc11771c161b094a95b34b34
Youmi Kim <99166914+youmikimm@users.noreply.github.com>
2025-10-04 16:24:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1016 ms, Memory: 106292 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,15 +1,21 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())   # n은 표의 크기, m은 합을 구해야 하는 횟수
-table = [[0 for i in range(n+1)] for j in range(n+1)]
-sumTable = [[0 for i in range(n+1)] for j in range(n+1)]
+input = sys.stdin.readline
 
-for i in range(1, n+1):
-    table[i] = [0] + list(map(int, sys.stdin.readline().split()))
+N, M = list(map(int, input().split())) # N x N 배열, M개의 질의
+A = [[0] * (N+1)]
+D = [[0] * (N+1) for _ in range(N+1)]
 
-for i in range(1, n+1):  # 구간 합 sumTable 채우기
-    for j in range(1, n+1):
-        sumTable[i][j] = table[i][j] + sumTable[i][j-1] + sumTable[i-1][j] - sumTable[i-1][j-1]
+# 2차원 배열(원 데이터) 만들기
+for _ in range(N):
+    A_row = [0] + list(map(int, input().split()))
+    A.append(A_row)
 
-for i in range(m):
-    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
-    print(sumTable[x2][y2]-sumTable[x2][y1-1]-sumTable[x1-1][y2] + sumTable[x1-1][y1-1])
+# 덧셈 배열 만들기
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j]
+
+# 질의에 대한 답 리턴
+for _ in range(M):
+	sx, sy, ex, ey = map(int, input().split())
+	print(D[ex][ey] - D[sx-1][ey] - D[ex][sy-1] + D[sx-1][sy-1])
